b75d9b3f176ea26ad7a888e42fd0548b3dc55a0e
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2026-04-08 17:53:59 -0300

    Add security config and authentication service
    Introduce basic Spring Security setup and a UserDetails-based authentication service. SecurityConfig registers a SecurityFilterChain bean that disables CSRF, sets session management to STATELESS, and permits all requests. UserRepository now exposes findByLogin returning UserDetails. Added AutenticationService implementing UserDetailsService to load users via the repository.

---
+++ src/main/java/com/kalebzaki/syncspace/service/AutenticationService.java
@@ -0,0 +1,25 @@
+package com.kalebzaki.syncspace.service;
+
+import com.kalebzaki.syncspace.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AutenticationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AutenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
+    }
+
+
+
+}
